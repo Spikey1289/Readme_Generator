abc489baf225bee34c710941f84f6f020f23fc61
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-01-25 20:43:54 -0500

    finished, updated generation, fixed bugs

--- index.js
+++ index.js
@@ -1,13 +1,106 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//object of questions for user input
+const questions = {
+    introduction: "-Hi! welcome to Spikey1289's README Generator. Please answer the following questions to generate your README file-\n\n",
+    newLine: "\n-Please remember hitting enter/return will end this question instead of making a new line.-\n-If you need a new line '\\n' will make a new line-\n\n",
+    listSelect: "\n-you can use the arrow keys and enter to select a license-\n\n",
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    title: "What is the title of the project: ",
+    description: "Please enter a brief description of your project: ",
+    instructions: "Please enter your instructions: ",
+    usage: "Please enter the software/app's intended use: ",
+    contributing: "Please enter a way for people to contribute to your work: ",
+    testing: "Please enter the tests that will be needed to pass to allow contributions: ",
+    license: "Please pick the type of license you would like to use from the following selection: ",
+    github: "Please enter your Github username so that others may contact you if they have questions or comments:  ",
+    email: "Please enter your email so that others may contact you if they have questions or comments: "
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+//function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.log(err) : console.log('Successfully created README')
+    );
+}
+
+//function to initialize app
+function init() {
+    let data = {};
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions.introduction + questions.title,
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions.newLine + questions.description,
+                name: 'description'
+            },
+            {
+                type: 'input',
+                message: questions.newLine + questions.instructions,
+                name: 'instructions'
+            },
+            {
+                type: 'input',
+                message: questions.newLine + questions.usage,
+                name: 'usage'
+            },
+            {
+                type: 'input',
+                message: questions.newLine + questions.contributing,
+                name: 'contributing'
+            },
+            {
+                type: 'input',
+                message: questions.newLine + questions.testing,
+                name: 'testing'
+            },
+            {
+                type: 'list',
+                message: questions.listSelect + questions.license,
+                name: 'license',
+                choices: [
+                    'none', 
+                    'MIT License', 
+                    'Apache License 2.0', 
+                    'ISC License', 
+                    'BSD 3-Clause "New" or "Revised" License', 
+                    'BSD 2-Clause "Simplified" License'
+                ],
+            },
+            {
+                type: 'input',
+                message: questions.github,
+                name: 'github'
+            },
+            {
+                type: 'input',
+                message: questions.email,
+                name: 'email'
+            }
+
+        ]).then((response) => {
+            data.title = response.title;
+            data.description = response.description;
+            data.instructions = response.instructions;
+            data.usage = response.usage;
+            data.contributing = response.contributing;
+            data.testing = response.testing;
+            data.license = response.license;
+            data.github = response.github;
+            data.email = response.email;
+            
+            writeToFile('README_OUTPUT.md', data);
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,147 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//Creates a function that returns a license badge based on which license is passed in
+//If there is no license, return an empty string
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  switch(true){
+    case license === 'MIT License':
+      return '\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n'
+    break;
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    case license === 'Apache License 2.0':
+      return '\n[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n'
+    break;
+
+    case license === 'ISC License':
+      return '\n[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)\n'
+    break;
+
+    case license === 'BSD 3-Clause "New" or "Revised" License':
+      return '\n[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n'
+    break;
+
+    case license === 'BSD 2-Clause "Simplified" License':
+      return '\n[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)\n'
+    break;
+
+    case license === 'none':
+      return '';
+      break;
+
+    default:
+      return '';
+    break
+  }
+}
+
+//Creates a function that returns the license link
+//If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+  switch (true) {
+    case license === 'MIT License':
+      return '\n### License\n  \nThis Application is covered by the MIT License. For more information please follow this link to the [MIT License](https://opensource.org/licenses/MIT)\n'
+      break;
+
+    case license === 'Apache License 2.0':
+      return '\n### License\n  \nThis Application is covered by the Apache License 2.0. For more information please follow this link to the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0)\n'
+      break;
+
+    case license === 'ISC License':
+      return '\n### License\n  \nThis Application is covered by the ISC License. For more information please follow this link to the [ISC License](https://opensource.org/licenses/ISC)\n'
+      break;
+
+    case license === 'BSD 3-Clause "New" or "Revised" License':
+      return '\n### License\n  \nThis Application is covered by the BSD 3-Clause "New" or "Revised" License. For more information please follow this link to the [BSD 3-Clause "New" or "Revised" License](https://opensource.org/licenses/BSD-3-Clause)\n'
+      break;
+
+    case license === 'BSD 2-Clause "Simplified" License':
+      return '\n### License\n  \nThis Application is covered by the BSD 2-Clause "Simplified" License. For more information please follow this link to the [BSD 2-Clause "Simplified" License](https://opensource.org/licenses/BSD-2-Clause)\n'
+      break;
+
+    case license === 'none':
+      return '';
+      break;
+    
+    default:
+      return '';
+      break;
+  }
+}
+
+//Creates a function that returns the license section of README
+//If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(license != 'none') {
+    return '- [License](#license)\n';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let markdown = "";
 
-`;
+  //add title from user
+  markdown += `# ${data.title}\n`;
+
+  //add license img/btn
+  markdown += renderLicenseBadge(data.license)+ '\n';
+  
+  //add TOC Header
+  markdown += '#### Table of Contents\n'
+  //add TOC
+  markdown += `
+  - [Description](#description)
+  - [Instructions](#instructions)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Testing](#testing)
+  - [Questions](#questions)
+  ` + renderLicenseSection(data.license) + '\n\n';
+
+  //add description header
+  markdown += '## Description\n\n';
+
+  //add description text from user
+  markdown += `${data.description}\n\n`;
+
+  //add instructions Header
+  markdown += `## Instructions\n\n`;
+
+  //add instructions text from user
+  markdown += `${data.instructions}\n\n`;
+
+  //add usage Header
+  markdown += `## Usage\n\n`;
+
+  //add usage text from user
+  markdown += `${data.usage}\n\n`;
+
+  //add contributing header
+  markdown += `### Contributing\n\n`
+
+  //add contributing text from user
+  markdown += `${data.contributing}\n\n`;
+
+  //add tests header
+  markdown += '### Testing\n\n';
+
+  //add tests text from user
+  markdown += `${data.testing}\n\n`;
+
+  //adds heading for contact info
+  markdown += `### Questions\n\n`
+
+  //adds contact information
+  markdown += `- My Github: [`+ data.github + `](https://github.com/` + data.github + `) \n `;
+  markdown += `- My Email Address: [` + data.email + `](mailto:`+ data.email +`)\n\n`;
+
+  //adds license information if any was selected
+  markdown += renderLicenseLink(data.license);
+
+  //returns the completed markdown string for use in the markdown file
+  return markdown;
 }
 
 module.exports = generateMarkdown;
